a53e391e7d8585f4738bc50c4b536f39787521a1
Thomas Smyth <tom@tomsmyth.ca>
2014-01-06 14:02:09 -0500

    added migration

---
+++ db/migrate/20140106182253_move_goal_links_back_to_triggers.diaedu.rb
@@ -0,0 +1,13 @@
+# This migration comes from diaedu (originally 20140106155143)
+class MoveGoalLinksBackToTriggers < ActiveRecord::Migration
+  def up
+
+    execute("UPDATE diaedu_kb_links l SET obj1_id = l2.obj1_id
+      FROM diaedu_kb_objs b, diaedu_kb_links l2
+      WHERE l.obj1_id = b.id AND b.type = 'Diaedu::Barrier' AND l2.obj2_id = b.id")
+
+  end
+
+  def down
+  end
+end
