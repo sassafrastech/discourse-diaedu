dab54f2e99798f656b4b8e51ade41dfdeeb28ad2
Thomas Smyth <tom@tomsmyth.ca>
2014-01-31 10:59:46 -0500

    configured active_admin dashboard (#553)

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,31 +3,18 @@ ActiveAdmin.register_page "Dashboard" do
   menu :priority => 1, :label => proc{ I18n.t("active_admin.dashboard") }
 
   content :title => proc{ I18n.t("active_admin.dashboard") } do
-    div :class => "blank_slate_container", :id => "dashboard_default_message" do
-      span :class => "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+
+    # recent objs
+    columns do
+      column do
+        panel "Recent Submissions" do
+          ul do
+            Diaedu::KbObj.recent_unapproved(20).map do |obj|
+              li link_to("[#{obj.type.demodulize}] #{obj.name}", send("edit_#{obj.class.admin_route_key}_path", obj))
+            end
+          end
+        end
       end
     end
-
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
-
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
   end # content
 end
