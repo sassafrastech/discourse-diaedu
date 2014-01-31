69a134d2a8797749457a9303a1932980bf98ac0e
Thomas Smyth <tom@tomsmyth.ca>
2014-01-31 09:52:36 -0500

    fixed active_admin authentication method (#553)

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -55,7 +55,7 @@ ActiveAdmin.setup do |config|
   #
   # This setting changes the method which Active Admin calls
   # within the controller.
-  config.authentication_method = :ensure_logged_in
+  config.authentication_method = :ensure_admin_logged_in
 
   # == User Authorization
   #
