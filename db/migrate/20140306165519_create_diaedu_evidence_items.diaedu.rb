dbe1565fc6cf900dca706ed8611dc317a504dd0b
Thomas Smyth <tom@tomsmyth.ca>
2014-03-06 11:57:01 -0500

    added evidence items migration

---
+++ db/migrate/20140306165519_create_diaedu_evidence_items.diaedu.rb
@@ -0,0 +1,16 @@
+# This migration comes from diaedu (originally 20140306165232)
+class CreateDiaeduEvidenceItems < ActiveRecord::Migration
+  def change
+    create_table :diaedu_evidence_items do |t|
+      t.integer :kb_obj_id
+      t.string :kind, :null => false
+      t.string :title
+      t.text :url
+      t.string :filename
+
+      t.timestamps
+    end
+    add_index :diaedu_evidence_items, :kb_obj_id
+    add_foreign_key :diaedu_evidence_items, :diaedu_kb_objs, :column => 'kb_obj_id'
+  end
+end
